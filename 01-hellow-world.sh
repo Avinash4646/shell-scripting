23eae5790af62fdd28fdedb196a997941bd26699
Avinash4646 <avinash.vemuri1994@gmail.com>
2025-08-12 20:01:14 +0530

    changes pushing

--- 01-hellow-world.sh
+++ 01-hellow-world.sh
@@ -3,7 +3,14 @@
 NUMBER1=$1
 NUMBER2=$2
 
+R="\e[31m"
+
 SUM=$((NUMBER1+NUMBER2))
 
-echo "Addition of two number is: $SUM"
+if [$SUM -eq 90]
 
+echo -e "It is a good sign ... $R$SUM"
+else
+
+echo -e "It is not a good sign .... $R$SUM"
+fi
