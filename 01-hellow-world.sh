c4084d44bfbdbd34fb3821e5c7c85d7a6a941c28
Avinash4646 <avinash.vemuri1994@gmail.com>
2025-09-02 15:57:16 +0530

    changes pushing

--- 01-hellow-world.sh
+++ 01-hellow-world.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+PERSON1=Ramesh
+PERSON2=Suresh
+
 echo "Hello world, I am learing Devops with AWS"
 
-echo "Ramesh: what are you doing?"
-echo "Suresh: i am doing good, how about you"
-echo " Ramesh: nothing"
-echo "Suresh: nothing from my side"
+echo "$PERSON1: what are you doing?"
+echo "$PERSON2: i am doing good, how about you"
+echo "$PERSON1: nothing"
+echo "$PERSON2: nothing from my side"
 
