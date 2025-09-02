4cebfb43a04b83cf8d8b4902d41b0561959096a4
Avinash4646 <avinash.vemuri1994@gmail.com>
2025-09-02 18:58:02 +0530

    changes pushing

--- 12-loops.sh
+++ 12-loops.sh
@@ -4,3 +4,11 @@ for i in $@
 do
     yum install $i -y
 done
+
+# Improvements
+# 1- implement log files
+# 2- implement colors
+# 3- implement validations
+# 4- implement validations through functions
+# 5- your script should check package is already installed or not, if already installed print yellow color
+# 6- if installed just print package is already installed, it should not run install command
