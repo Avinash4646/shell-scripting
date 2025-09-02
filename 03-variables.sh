12df9f54e2777f779609b41deb700be9fdeb144c
Avinash4646 <avinash.vemuri1994@gmail.com>
2025-09-02 16:02:36 +0530

    changes pushing

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "Hello world, I am learing Devops with AWS"
+
+echo "$PERSON1: what are you doing?"
+echo "$PERSON2: i am doing good, how about you"
+echo "$PERSON1: nothing"
+echo "$PERSON2: nothing from my side"
