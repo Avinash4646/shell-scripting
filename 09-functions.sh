9687c777a0ab9fb6075db5d1b720a8e94fe6fe8e
Avinash4646 <avinash.vemuri1994@gmail.com>
2025-09-02 18:33:23 +0530

    changes pushing

--- 09-functions.sh
+++ 09-functions.sh
@@ -14,10 +14,10 @@ VALIDATE(){
     #$1 --> it will receive the argument
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 ....... $RED failure $RESTCOLOR"
+        echo -e "$2 ....... $RED failure $RESETCOLOR"
         exit 1
     else
-        echo -e "$2 ....... $GREEN success $RESTCOLOR"
+        echo -e "$2 ....... $GREEN success $RESETCOLOR"
     fi
 }
 
