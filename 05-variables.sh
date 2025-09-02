a2497826074a8e194d47a9f27586686e812b11a1
Avinash4646 <avinash.vemuri1994@gmail.com>
2025-09-02 16:12:52 +0530

    changes pushing

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username"
 read USERNAME # This is the variable where the value of the one user entered
 
 echo "Username entered is: $USERNAME"
+
+echo "Please enter your password"
+
+read PASSWORD
+
+echo "Password entered is: $PASSWORD"
